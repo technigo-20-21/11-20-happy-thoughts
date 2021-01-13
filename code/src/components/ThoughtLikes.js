5ea28fcb4d4c3def7529c308773c9f871224ce73
Evelina Berg <evelina.berg@outlook.com>
2021-01-13 14:39:42 +0100

    Fetch URL for likes added

--- code/src/components/ThoughtLikes.js
+++ code/src/components/ThoughtLikes.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { LIKES_URL } from '../urls';
 
 export const ThoughtLikes = ({ id, onLikedThought }) => {
+  const handleLikedThought = () => {
+    fetch(LIKES_URL,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(() => {
+      onLikedThought(id);
+    });
+
   return (
-    <button type="button" onClick={() => onLikedThought(id)} className="liked-button">
+    <button type="button" onClick={handleLikedThought} className="liked-button">
       <span role="img" aria-label="red heart" className="heart">❤️</span>
     </button>
   )
--- code/src/urls.js
+++ code/src/urls.js
@@ -1,1 +1,3 @@
 export const THOUGHTS_URL = 'https://happiest-happy-thoughts.herokuapp.com/thoughts'
+
+export const LIKES_URL = `https://happiest-happy-thoughts.herokuapp.com/thoughts/${id}/likes`
