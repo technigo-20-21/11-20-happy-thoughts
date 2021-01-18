3df28eec746542102d4fbb0f46efd522e796b554
Evelina Berg <evelina.berg@outlook.com>
2021-01-18 22:14:11 +0100

    Revert "Fetch URL for likes added"
    This reverts commit 5ea28fcb4d4c3def7529c308773c9f871224ce73.

--- code/src/components/Thought.js
+++ code/src/components/Thought.js
@@ -8,27 +8,13 @@ export const Thought = ({
   id,
   onLikedThought
 }) => {
-  const [likedThought, setLikedThougt] = useState([]);
-
-  const getLikedThought = (id) => {
-    const updatedThoughts = thoughts.map((thought) => {
-      if (id === thought._id) {
-        thought.hearts += 1;
-      }
-      return thought;
-    });
-    setThoughts(updatedThoughts);
-    if (!likedThought.includes(id)) {
-      setLikedThougt([...likedThought, id]);
-    }
-  };
   return (
     <div className="thought-container">
       <p className="thought" id={id}>
         {message}
       </p>
       <div className="liked-container">
-        <ThoughtLikes id={id} onLikedThought={() => onLikedThought(id)} />
+        <ThoughtLikes id={id} onLikedThought={onLikedThought} />
         <p className="liked"> x {hearts}</p>
       </div>
       <p className="thought-time">{time}</p>
--- code/src/components/ThoughtLikes.js
+++ code/src/components/ThoughtLikes.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { LIKES_URL } from '../urls';
 
-export const onLikedThought = (id) => {
-  fetch(`${LIKES_URL}`, {
-    method: 'POST',
-    body: '',
-    headers: { 'Content-Type': 'application/json' }
-  }).then(() => getLikedThought(id));
-
+export const ThoughtLikes = ({ id, onLikedThought }) => {
   return (
     <button type="button" onClick={() => onLikedThought(id)} className="liked-button">
       <span role="img" aria-label="red heart" className="heart">❤️</span>
